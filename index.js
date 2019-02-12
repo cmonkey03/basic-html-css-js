53382077058be9d9fc9189b351902e780369f9fa
github username <github email address>
2019-02-12 12:36:06 -0500

    Added DOM load event listener

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-var middleLi = document.getElementsByClassName('intro')
-console.log(middleLi)
+document.addEventListener("DOMContentLoaded", console.log('hello'))
+
+var intro = document.getElementsByClassName('intro')
+var second = document.getElementById('second')
+
+console.log(intro, second)
