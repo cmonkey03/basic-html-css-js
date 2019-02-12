2e8c38c5e2799727d68d2a700c5455a27eb42b37
github username <github email address>
2019-02-12 12:46:56 -0500

    Added psuedo-element

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ document.addEventListener("DOMContentLoaded", console.log('hello'))
 var intro = document.getElementsByClassName('intro')
 var second = document.getElementById('second')
 
-console.log(intro, second)
+console.log(intro)
